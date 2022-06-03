09263c5d531d7204e962fe86c2999088e685394a
KChi <59391215+KagChi@users.noreply.github.com>
2022-06-03 17:51:54 +0700

    feat: initial commit

---
+++ src/error.ts
@@ -0,0 +1,28 @@
+import { makeCordisError } from "@cordis/error";
+
+/**
+ * @internal
+ */
+export const CordisBrokerError = makeCordisError(
+    Error,
+    {
+        brokerNotInit: "Failed to complete operation, broker has not yet been initalized",
+        noResponseInTime: (waitingFor: number) => `Did not recieve a response in time, waited for ${waitingFor}`,
+        serverFailure: "Server failed with handling the request"
+    }
+);
+
+/**
+ * @internal
+ */
+export const CordisBrokerTypeError = makeCordisError(
+    TypeError,
+    {
+        missingProperties: (props: string[] | string) => `Given payload is missing required data, ${
+            Array.isArray(props)
+                ? props.map(e => `"${e}"`).join(", ")
+            /* istanbul ignore next */
+                : `"${props}"`
+        }`
+    }
+);
